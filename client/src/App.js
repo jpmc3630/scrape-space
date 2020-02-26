0ad61d1ee06dba494791eafe31884eef5ce27f3a
James McGlone <jamespmcglone@gmail.com>
2020-02-27 02:37:50 +0800

    basics

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from 'axios';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      data: [],
+
+    };
+ }
+
+
+ componentDidMount() {
+
+  this.loadTest();
+}
+
+  loadTest = () => {
+    axios
+    .get(
+      `/test`
+    )
+    .then(({ data }) => {
+        this.setState({data}); 
+          console.log(data);
+          });
+}
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
